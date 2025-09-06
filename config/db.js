9a48548b9312df9ad6621a932909c1a6727c73ab
abdieldeathayde <abdieldeathayde67@gmail.com>
2025-09-06 20:16:19 -0300

    render

--- config/db.js
+++ config/db.js
@@ -2,14 +2,15 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: "mysql.railway.internal",
+  host: "tramway.proxy.rlwy.net",
   user: "root",
   password: "KCchXIbdAFNMwPvXxFVZjdRmETTvnAGO",
   database: "railway",
-  port: 3306,
+  port: 36360,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool.promise(); // Para usar async/await
+"
