7a7a12d571ebf955ae32698b6f3bce7658b54c6b
abdieldeathayde <abdieldeathayde67@gmail.com>
2025-09-06 12:00:55 -0300

    adicionando gitignore

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME_FORM,
-  port: process.env.DB_PORT,
+  host: "mysql.railway.internal",
+  user: "root",
+  password: "KCchXIbdAFNMwPvXxFVZjdRmETTvnAGO",
+  database: "railway",
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
