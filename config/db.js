cfc3ae30dfa9877fd251b134999648d3a19e45a3
abdieldeathayde <abdieldeathayde67@gmail.com>
2025-09-11 13:12:03 -0300

    teste3

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-<<<<<<< HEAD
   host: process.env.DB_HOST,     
   port: process.env.DB_PORT,     
   user: process.env.DB_USER,     
@@ -12,17 +11,3 @@ const pool = mysql.createPool({
 
 
 module.exports = pool.promise();
-=======
-  host: "tramway.proxy.rlwy.net",
-  user: "root",
-  password: "KCchXIbdAFNMwPvXxFVZjdRmETTvnAGO",
-  database: "railway",
-  port: 36360,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-module.exports = pool.promise(); // Para usar async/await
-"
->>>>>>> parent of 4f9cde7 (teste)
