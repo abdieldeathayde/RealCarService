6a6bad9cd27aa3efb20139fd3caa18720d1ffec1
abdieldeathayde <abdieldeathayde67@gmail.com>
2025-09-11 13:09:45 -0300

    removendo dado

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
+<<<<<<< HEAD
   host: process.env.DB_HOST,     
   port: process.env.DB_PORT,     
   user: process.env.DB_USER,     
@@ -11,3 +12,17 @@ const pool = mysql.createPool({
 
 
 module.exports = pool.promise();
+=======
+  host: "tramway.proxy.rlwy.net",
+  user: "root",
+  password: "KCchXIbdAFNMwPvXxFVZjdRmETTvnAGO",
+  database: "railway",
+  port: 36360,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool.promise(); // Para usar async/await
+"
+>>>>>>> parent of 4f9cde7 (teste)
